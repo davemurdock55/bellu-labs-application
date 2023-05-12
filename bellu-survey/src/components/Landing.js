8988632a8ff81945ce3188e2716907045c5275fd
davemurdock55 <davemurdock55@gmail.com>
2023-05-12 13:41:34 -0600

    Added some styling for the navlinks

--- bellu-survey/src/components/Landing.js
+++ bellu-survey/src/components/Landing.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Landing ()
 {
 	return (
-		<div className="flex justify-center items-center h-1/2">
-			<div className="text-center">
-				<h1 className="text-3xl font-bold mb-4">Landing</h1>
-				<p className="text-gray-600">This is the landing page</p>
+		<div className="flex justify-center items-center h-1/2 p-10">
+			<div>
+				<h1 className="text-center text-3xl font-bold mb-4">Congratulations!</h1>
+				<div className="text-center">
+					<p className="text-gray-600">
+						You are one step closer to achieving the happier, healthier hair of your dreams! We are excited to tell you what we learned about your hair.
+					</p>
+					<p className="text-gray-600">
+						Remember, this report is in depth (AKA very long and sciencey). Feel free to take a break from reading before coming back to the report if you need one. Before we get started, let’s briefly go over the content for the in-depth analysis.
+					</p>
+					<p className="text-gray-600">
+						In this report we will give you a little refresher on hair anatomy.<br /><br />
+						Then, we’ll show you how your hair health compares to other Bellu customers.
+					</p>
+					<p className="text-gray-600">
+						Next, we’ll show you what we learned from the micrographs (microscopic images) of your hair cuticle. We’ll also throw in a few images of your hair so you can see what we are seeing.
+					</p>
+					<p className="text-gray-600">
+						Finally, we will repeat our product recommendations to you and give you even more detail about the lifestyle recommendations.
+					</p>
+					<div style={ { display: "flex", flexDirection: "column", alignItems: "center" } }>
+						<Link to="/hair-and-scalp" className="my-6 p-4 bg-[#e1c2bd] text-white font-bold">Let’s get into it!</Link>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- bellu-survey/src/components/Navbar.js
+++ bellu-survey/src/components/Navbar.js
@@ -1,21 +1,57 @@
+import { NavLink } from 'react-router-dom';
+
 export default function Navbar ()
 {
+	const activeClass = 'active-navlink';
+
 	return (
 		<>
 			<nav className="bg-[#e1c2bd]">
-				<div className="flex items-center justify-center">
-					<img src="/images/bellu-labs-1.png" alt="Bellu Labs Logo" className="h-12 w-12" />
-					<h1 className="m-7 text-white font-medium logo text-5xl">bellu labs</h1>
-				</div>
-
+				<NavLink to="/" className="flex items-center justify-center">
+					<img
+						src="/images/bellu-labs-1.png"
+						alt="Bellu Labs Logo"
+						className="h-12 w-12"
+					/>
+					<h1 className="m-7 text-white font-medium logo text-5xl">
+						bellu labs
+					</h1>
+				</NavLink>
 			</nav>
-			<div className="flex flex-row justify-center space-x-4 p-4 h-32">
-				<div className="px-3 flex items-center justify-center border-2 border-solid border-[##778996] basis-2/12">Hair & Scalp</div>
-				<div className="px-3 flex items-center justify-center border-2 border-solid border-[##778996] basis-6/12">Lifestyle</div>
-				<div className="px-3 flex items-center justify-center border-2 border-solid border-[##778996] basis-3/12">History</div>
-				<div className="px-3  flex items-center justify-center border-2 border-solid border-[##778996] basis-1/12">Goals & Preferences</div> {/* changed from Goals & Preferences to make it fit better */ }
+			<div className="flex flex-row justify-center space-x-4 p-4 pt-2 h-20 text-md font-medium text-[#778996]">
+				<NavLink
+					to="/hair-and-scalp"
+					className={ ({ isActive }) =>
+						isActive ? `${activeClass} px-3 flex items-center justify-center border-2 border-solid border-[#778996] basis-2/12` : 'px-3 flex items-center justify-center border-2 border-solid border-[#778996] basis-2/12'
+					}
+				>
+					Hair & Scalp
+				</NavLink>
+				<NavLink
+					to="/lifestyle"
+					className={ ({ isActive }) =>
+						isActive ? `${activeClass} px-3 flex items-center justify-center border-2 border-solid border-[#778996] basis-6/12` : 'px-3 flex items-center justify-center border-2 border-solid border-[#778996] basis-6/12'
+					}
+				>
+					Lifestyle
+				</NavLink>
+				<NavLink
+					to="/history"
+					className={ ({ isActive }) =>
+						isActive ? `${activeClass} px-3 flex items-center justify-center border-2 border-solid border-[#778996] basis-3/12` : 'px-3 flex items-center justify-center border-2 border-solid border-[#778996] basis-3/12'
+					}
+				>
+					History
+				</NavLink>
+				<NavLink
+					to="/goals-and-preferences"
+					className={ ({ isActive }) =>
+						isActive ? `${activeClass} px-3 flex items-center justify-center border-2 border-solid border-[#778996] basis-1/12` : 'px-3 flex items-center justify-center border-2 border-solid border-[#778996] basis-1/12'
+					}
+				>
+					Goals
+				</NavLink>
 			</div>
-
 		</>
 	);
 }
